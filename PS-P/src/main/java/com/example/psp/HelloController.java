6f7b7425110efdb1638932491ffba59ba4cb8446
Blewke <julesbloux@gmail.com>
2023-03-13 10:11:36 +0100

    add javafx

--- PS-P/src/main/java/com/example/psp/HelloController.java
+++ PS-P/src/main/java/com/example/psp/HelloController.java
@@ -2,6 +2,7 @@ package com.example.psp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.web.WebView;
 
 public class HelloController {
     @FXML
--- PS-P/src/main/java/module-info.java
+++ PS-P/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.psp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens com.example.psp to javafx.fxml;
